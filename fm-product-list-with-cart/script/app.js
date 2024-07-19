1b135f7d2c2e958fa7765d4639675a94cb2b5de9
Kajet <kajetanslega@gmail.com>
2024-07-19 19:14:10 +0200

    edit

--- fm-product-list-with-cart/script/app.js
+++ fm-product-list-with-cart/script/app.js
@@ -20,9 +20,8 @@ for (let i = 0; i < 9; i++) {
         prod.append(imgBtn);
         imgBtn.append(img);
         imgBtn.append(btn);
-        btn.classList.add("cart-button", "text-preset-4-bold");
+        btn.classList.add(`cart-button${i}`, "text-preset-4-bold");
         btn.innerHTML = '<img src="./assets/images/icon-add-to-cart.svg" style="position: relative; top: 3px; " /> <span class="add-to-card">Add to Cart</span>';
-
         prod.append(itemInfo);
         img.src = product[i].image.desktop;
         img.classList.add("product-image");
@@ -35,6 +34,34 @@ for (let i = 0; i < 9; i++) {
         itemInfo.append(price);
         price.classList.add("text-preset-3", "price");
         price.innerText = `$${product[i].price.toFixed(2)}`;
+
+        let quantity = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+        let buttonClick = document.querySelector(`.cart-button${i}`);
+        buttonClick.addEventListener("click", function () {
+          btn.classList.add("btn-clicked");
+          btn.innerHTML = `<div class="flex-button decrement"><img src="./assets/images/icon-decrement-quantity.svg" style="position: relative; ";" /></div> <span class="add-to-card unit">${quantity[i]}</span> <div class="flex-button increment"><img src="./assets/images/icon-increment-quantity.svg" style="position: 
+            relative;  ";" /></div>`;
+          let decreaseBtn = document.querySelector(".decrement");
+          let increaseBtn = document.querySelector(".increment");
+          let unitSpan = document.querySelector(".unit");
+          if (quantity[i] > 0) {
+            decreaseBtn.addEventListener("click", function () {
+              quantity[i]--;
+              console.log(quantity);
+              unitSpan.innerText = quantity[i];
+            });
+            increaseBtn.addEventListener("click", function () {
+              quantity[i]++;
+              console.log(quantity);
+              unitSpan.innerText = quantity[i];
+            });
+          } else {
+            quantity[i]++;
+            btn.classList.toggle("btn-clicked");
+            btn.innerHTML = '<img src="./assets/images/icon-add-to-cart.svg" style="position: relative; top: 3px; " /> <span class="add-to-card">Add to Cart</span>';
+          }
+        });
       }
     })
     .catch((err) => {
